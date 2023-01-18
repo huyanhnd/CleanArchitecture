c30546c4bd3db641521c227a3ae1903835317253
aiden.nguyen <aiden.nguyen@avepoint.com>
2023-01-18 15:11:19 +0700

    Add Jwt Token

---
+++ CleanArchitecture/CleanArchitecture.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Common.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        User? GetUserByEmail(string email);
+        void Add(User user);
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,68 @@
+﻿using CleanArchitecture.Application.Common.Interfaces.Authentication;
+using CleanArchitecture.Application.Common.Interfaces.Persistence;
+using CleanArchitecture.Domain.Entities;
+using System.Runtime.CompilerServices;
+
+namespace CleanArchitecture.Application.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
+        }
+
+        public AuthenticationResult Login(string email, string password)
+        {
+            // Validate the user exist 
+            if (_userRepository.GetUserByEmail(email) is not User user)
+            {
+                throw new Exception("User with given email doesn't exist.");
+            }
+
+            // Validate the password is correct
+            if (user.Password != password)
+            {
+                throw new Exception("Invalid password.");
+            }
+
+            // Create JWT token
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthenticationResult(
+                    user,
+                    token);
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            // Validate the user doesn't exist
+            if (_userRepository.GetUserByEmail(email) is not null)
+            {
+                throw new Exception("User with given email already exist.");
+            }
+
+            // Create user (generate unique ID) & persist to DB
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password
+            };
+
+            _userRepository.Add(user);
+
+            // Create JWT Token
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthenticationResult(
+                user,
+                token);
+        }
+    }
+}
